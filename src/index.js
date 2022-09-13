82693dc0f09e3d4a88326f652f17348e1a96c973
Lautaro Rocha <lautarovrocha@gmail.com>
2022-09-13 14:37:36 -0300

    build directory

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcax84gtMtmxqWVaWuXku9o454nLKdIqU",
+  authDomain: "minote-offline.firebaseapp.com",
+  projectId: "minote-offline",
+  storageBucket: "minote-offline.appspot.com",
+  messagingSenderId: "1001880281920",
+  appId: "1:1001880281920:web:7aa99444257efbbbc710a5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
